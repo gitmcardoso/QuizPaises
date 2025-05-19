7639d2e664d61b378c091cea3ae00ea64907ea84
gitmcardoso <matheus.cbatista7@gmail.com>
2025-05-19 10:56:29 -0300

    Update app.js

--- Projeto2025/jogo/app.js
+++ Projeto2025/jogo/app.js
@@ -118,7 +118,7 @@ async function obterNovaBandeira() {
       ranking.push(userData);
       localStorage.setItem('ranking', JSON.stringify(ranking));
 
-      fetch('https://609a-200-206-76-106.ngrok-free.app/salvarRanking', {
+      fetch('https://871f-200-206-76-106.ngrok-free.app/salvarRanking', {
         method: 'POST',
         headers: {
           'ngrok-skip-browser-warning': 'true', 
